582028571cfd74724fd7c2c2dfad0f07c2c89f55
Jhonatas Rodrigues da Silva Santos <jhonatasrsilvasantos@gmail.com>
2021-04-23 23:29:25 -0300

    adding app bar with degrader for avatar and text

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,3 +1,4 @@
+import 'package:Developer_Quiz/home/home_page.dart';
 import 'package:Developer_Quiz/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 
@@ -6,7 +7,7 @@ class AppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Developer_Quiz",
-      home: SplashPage(),
+      home: HomePage(),
     );
   }
 }
